c0bc11f568ed6d62729bd8542562eacc166f592e
Marta Kozłowska <screamek@Martas-MacBook-Pro.local>
2020-02-04 21:37:19 -0700

    vpos.py

--- assignments/01_strings/vpos.py
+++ assignments/01_strings/vpos.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Author : Marta Kozlowska
-Date   : 01.28.2020
-Purpose: What's in the water
+Author : Me <me@foo.com>
+Date   : today
+Purpose: Rock the Casbah
 """
 
 import argparse
@@ -15,25 +15,59 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Crows nest',
+        description='Rock the Casbah',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('water',
+    parser.add_argument('positional',
                         metavar='str',
-                        help='in the water')
+                        help='A positional argument')
+
+    parser.add_argument('-a',
+                        '--arg',
+                        help='A named string argument',
+                        metavar='str',
+                        type=str,
+                        default='')
+
+    parser.add_argument('-i',
+                        '--int',
+                        help='A named integer argument',
+                        metavar='int',
+                        type=int,
+                        default=0)
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='A readable file',
+                        metavar='FILE',
+                        type=argparse.FileType('r'),
+                        default=None)
+
+    parser.add_argument('-o',
+                        '--on',
+                        help='A boolean flag',
+                        action='store_true')
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
-    """What's in the water"""
+    """Make a jazz noise here"""
 
     args = get_args()
-    water = args.water
-    article = 'an' if water[0].lower() in 'aeiou' else 'a'
+    str_arg = args.arg
+    int_arg = args.int
+    file_arg = args.file
+    flag_arg = args.on
+    pos_arg = args.positional
 
-    print(f'Ahoy, Captain, {article} {water} off the larboard bow!')
+    print('str_arg = "{}"'.format(str_arg))
+    print('int_arg = "{}"'.format(int_arg))
+    print('file_arg = "{}"'.format(file_arg.name))
+    print('flag_arg = "{}"'.format(flag_arg))
+    print('positional = "{}"'.format(pos_arg))
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
