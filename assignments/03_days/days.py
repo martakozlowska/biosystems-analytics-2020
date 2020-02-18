43f2447ed836c975f1211267d798e0bb105fc98e
Marta Kozłowska <screamek@Martas-MacBook-Pro.local>
2020-02-17 19:35:08 -0700

    homework

--- assignments/03_days/days.py
+++ assignments/03_days/days.py
@@ -33,17 +33,17 @@ def main():
     args = get_args()
     days = args.days
     week = {
-                    'Monday': 'On Mondays I never go to work',
-                    'Tuesday': 'On Tuesdays I stay at home',
-                    'Wednesday': 'On Wednesdays I never feel inclined',
-                    'Thursday': 'On Thursdays, it’s a holiday',
-                    'Friday': 'And Fridays I detest',
-                    'Saturday': 'Oh, it’s much too late on a Saturday',
-                    'Sunday': 'And Sunday is the day of rest'
-            }
+        "Monday": "On Mondays I never go to work",
+        "Tuesday": "On Tuesdays I stay at home",
+        "Wednesday": "On Wednesdays I never feel inclined",
+        "Thursday": "On Thursdays, it's a holiday",
+        "Friday": "And Fridays I detest",
+        "Saturday": "Oh, it's much too late on a Saturday",
+        "Sunday": "And Sunday is the day of rest"
+        }
 
-    for days in week:
-        print(week.get(days) if days in week else f"Can't find {days}")
+    for days in args.days:
+        print(week.get(days) if days in week else f'Can\'t find "{days}"')
 
 # --------------------------------------------------
 if __name__ == '__main__':
