0af081a6785a1595ac7085e477395233a7bd595e
Marta Kozłowska <screamek@dhcp-10-134-200-136.uawifi.arizona.edu>
2020-02-25 10:29:29 -0700

    toilet

--- exercises/07_wc/wc.py
+++ exercises/07_wc/wc.py
@@ -36,13 +36,31 @@ def get_args():
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """toilet time"""
 
     args = get_args()
+    total_lines = 0
+    total_words = 0
+    total_chars = 0
 
     for fh in args.file:
-        print(fh.readline(), end='')
+        num_lines = 0
+        num_words = 0
+        num_chars = 0
+        for line in fh:
+            num_lines += 1
+            words = line.split()
+            num_words += len(words)
+            num_chars += len(line)
 
+        total_lines += num_lines
+        total_words += num_words
+        total_chars += num_chars
+
+        print(f'{num_lines:8}{num_words:8}{num_chars:8} {fh.name}')
+
+    if len(args.file) > 1:
+        print(f'{total_lines:8}{total_words:8}{total_chars:8} total')
 
 # --------------------------------------------------
 if __name__ == '__main__':
