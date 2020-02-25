c63e13822f69020546d09d9220e6d7c09da31f95
Marta Kozłowska <screamek@dhcp-10-134-185-69.uawifi.arizona.edu>
2020-02-25 11:20:47 -0700

    homework

--- assignments/04_head/head.py
+++ assignments/04_head/head.py
@@ -18,55 +18,29 @@ def get_args():
         description='Head',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
+    parser.add_argument('file',
                         help='A readable file',
                         metavar='FILE',
                         type=argparse.FileType('r'),
                         default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument('-n',
+                        '--num',
+                        help='Number of lines to show',
+                        type=int,
+                        default='10')
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """head time"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
 
-    print('str_arg = "{}"'.format(str_arg))
-    print('int_arg = "{}"'.format(int_arg))
-    print('file_arg = "{}"'.format(file_arg.name))
-    print('flag_arg = "{}"'.format(flag_arg))
-    print('positional = "{}"'.format(pos_arg))
+    for fh in args.file:
+
+
 
 
 # --------------------------------------------------
