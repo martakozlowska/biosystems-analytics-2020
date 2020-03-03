f29df4f2c95dc9be81d3bbc83ce327b9560e5651
Marta Kozłowska <screamek@dhcp-10-134-248-34.uawifi.arizona.edu>
2020-03-03 10:50:14 -0700

    homework

---
+++ assignments/05_hamming/hamming.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+Author : MK
+Date   : 20.03.03
+Purpose: hamming time
+"""
+
+import argparse
+import os
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Hamming time',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        help='A readable file',
+                        metavar='FILE',
+                        type=argparse.FileType('r'),
+                        default=None)
+
+    parser.add_argument('-m',
+                        '--min',
+                        metavar=int,
+                        help='minimum integer value')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    for line in args.file:
+        line = line.strip()
+        word1, word2 = line.split()
+
+        if len(word1) == len(word2):
+            dist = 0
+            for c1, c2 in zip(word1, word2):
+                if c1 == c2:
+                    pass
+                else:
+                    dist += 1
+        else: #words not the same length
+            dist = 0
+            diff = abs(len(word1) - len(word2))
+            for c1, c2 in zip(word1, word2):
+                if c1 == c2:
+                    pass
+                else:
+                    dist += 1
+                diff += dist
+
+
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
