01f8e10d39fd420d75540d4cdef5fe6bd4ab6e45
Marta Kozłowska <screamek@Martas-MacBook-Pro.local>
2020-03-04 22:01:30 -0700

    homework

--- assignments/05_hamming/hamming.py
+++ assignments/05_hamming/hamming.py
@@ -2,7 +2,7 @@
 """
 Author : MK
 Date   : 20.03.03
-Purpose: hamming time
+Purpose: Hammer time
 """
 
 import argparse
@@ -15,7 +15,7 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Hamming time',
+        description='Hammer time',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('file',
@@ -26,7 +26,7 @@ def get_args():
 
     parser.add_argument('-m',
                         '--min',
-                        metavar=int,
+                        type=int,
                         default=0,
                         help='minimum integer value')
 
@@ -35,7 +35,7 @@ def get_args():
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Hammer time"""
 
     args = get_args()
 
@@ -51,18 +51,22 @@ def main():
                 else:
                     diff += 1
         else: #words not the same length
-            dist = 0
             diff = abs(len(word1) - len(word2))
+            dist = 0
             for c1, c2 in zip(word1, word2):
                 if c1 == c2:
                     pass
                 else:
                     dist += 1
-                diff += dist
+            diff += dist
 
         if args.min >= 0:
-
-            print(f'{diff:8}:{word1:20}{word2}')
+            if diff >= args.min:
+                print(f'{diff:8}:{word1:20}{word2:20}')
+            else:
+                None
+        else:
+            pass
 
 # --------------------------------------------------
 if __name__ == '__main__':
