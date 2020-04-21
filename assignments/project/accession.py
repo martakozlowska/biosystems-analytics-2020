592d3d3abaf6748b0fc36ac4e22d78d1318088c6
Marta Kozłowska <screamek@Martas-MacBook-Pro.local>
2020-04-21 13:48:37 -0700

    project

--- assignments/project/accession.py
+++ assignments/project/accession.py
@@ -18,50 +18,21 @@ def get_args():
         description='Find TAIR locus ID',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('-f',
-                        '--file',
-                        help='File to find TAIR locus ID',
+    parser.add_argument('-t',
+                        '--tair',
+                        help='File to find TAIR accession number',
                         metavar='FILE',
                         type=argparse.FileType('r'),
+                        nargs='+',
                         default=None,
                         required=True)
 
     parser.add_argument('-o',
                         '--output',
-                        help='Output file',
+                        help='Output file name',
                         metavar='FILE',
                         type=argparse.FileType('r'),
                         default='out.txt')
-
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
 
     return parser.parse_args()
 
@@ -71,17 +42,7 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
 
-    print('str_arg = "{}"'.format(str_arg))
-    print('int_arg = "{}"'.format(int_arg))
-    print('file_arg = "{}"'.format(file_arg.name))
-    print('flag_arg = "{}"'.format(flag_arg))
-    print('positional = "{}"'.format(pos_arg))
 
 
 # --------------------------------------------------
